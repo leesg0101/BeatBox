3e3fb35c4d149986133e4774f51ba508d5594ff5
leesg0101 <leesg0101@gmail.com>
2022-03-17 22:56:05 +0900

    Initial commit

---
+++ app/src/main/java/com/sg0101/android/beatbox/Sound.kt
@@ -0,0 +1,8 @@
+package com.sg0101.android.beatbox
+
+private const val WAV = ".wav"
+
+class Sound(val assetPath: String, var soundId: Int? = null) {
+
+    val name = assetPath.split("/").last().removeSuffix(WAV)
+}
---
+++ app/src/test/java/com/sg0101/android/beatbox/SoundViewModelTest.kt
@@ -0,0 +1,36 @@
+package com.sg0101.android.beatbox
+
+import org.junit.Assert.*
+import org.hamcrest.core.Is.`is`
+import org.hamcrest.MatcherAssert
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+
+class SoundViewModelTest {
+
+    private lateinit var beatBox: BeatBox
+    private lateinit var sound: Sound
+    private lateinit var subject: SoundViewModel
+
+    @Before
+    fun setUp() {
+        beatBox = mock(BeatBox::class.java)
+        sound = Sound("assetPath")
+        subject = SoundViewModel(beatBox)
+        subject.sound = sound
+    }
+
+    @Test
+    fun exposesSoundNameAsTitle() {
+        MatcherAssert.assertThat(subject.title, `is`(sound.name))
+    }
+
+    @Test
+    fun callsBeatBoxPlayOnButtonClicked() {
+        subject.onButtonClicked()
+        verify(beatBox).play(sound)
+    }
+
+}
